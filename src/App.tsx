d6682626ca817cc38520b330c5f862e17f986cb1
Theodore Abitbol <theodore.abitbol@gmail.com>
2023-12-27 15:27:53 +0100

    debuging for building completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import DatePicker from './component/DatePicker';
 
 function App() {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const handleDateChange = (date) => {
+  const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
-    // Handle the change (e.g., form state update)
+    // Additional logic for when the date changes
   };
 
-  const handleFormInputChange = (fieldName, value) => {
-    // Handle form input change
+  const handleFormInputChange = () => {
+    // Handle form input change. This function might be a placeholder if not used.
   };
 
   return (
-    <>
+    <div className="App">
       <DatePicker 
         id="my-datepicker"
         selectedDate={selectedDate}
         onChange={handleDateChange}
-        onFormInputChange={handleFormInputChange}
+        onFormInputChange={handleFormInputChange} // Providing this prop as required by DatePickerProps
       />
-    </>
+    </div>
   );
 }
 
