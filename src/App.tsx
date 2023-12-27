0565607ff73ccdfda26e369e11694ee0bfcb540e
Theodore Abitbol <theodore.abitbol@gmail.com>
2023-12-27 14:15:27 +0100

    CustomSelect's style updated and integrated directly

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
-import DatePicker from './component/DatePicker'
+import React, { useState } from 'react';
+import './App.css';
+import DatePicker from './component/DatePicker';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    // Handle the change (e.g., form state update)
+  };
+
+  const handleFormInputChange = (fieldName, value) => {
+    // Handle form input change
+  };
+
   return (
     <>
-      <DatePicker />
+      <DatePicker 
+        id="my-datepicker"
+        selectedDate={selectedDate}
+        onChange={handleDateChange}
+        onFormInputChange={handleFormInputChange}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
